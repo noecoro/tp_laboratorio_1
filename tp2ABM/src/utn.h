afe988f633eb6048013857de4c927dd7b33b3406
Noelia <52795935+noecoro@users.noreply.github.com>
2020-09-30 03:46:16 -0300

    Add files via upload

---
+++ tp2ABM/src/utn.h
@@ -0,0 +1,23 @@
+/*
+ * utn.h
+ *
+ *  Created on: 26 sept. 2020
+ *      Author: NOELIA
+ */
+
+#ifndef UTN_H_
+#define UTN_H_
+//===========================================
+int utn_getNumberInt(char* mensaje, char*mensajeError, int* pResultado, int reintentos,int maximo, int minimo);
+int utn_getName(char* mensaje, char* mensajeError, char* pResultado,int reintentos, int limite);
+
+int utn_getNumberFloat(char* mensaje, char*mensajeError,float* pResultado,int reintentos, float minimo,float maximo );
+//===========================================
+int utn_getInt(int* pResultado);
+//===========================================
+int utn_isNumber(char cadena[]);
+//===========================================
+int esUnNombreValido(char* cadena, int limite);
+
+
+#endif /* UTN_H_ */
